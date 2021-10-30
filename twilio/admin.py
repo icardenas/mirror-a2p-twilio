a6ee3f4ec094bef1f160635798e34f1798cad8c2
icardenas <ivan.cardenas101@gmail.com>
2021-10-30 13:20:19 -0400

    Apply Black formatter

--- twilio/admin.py
+++ twilio/admin.py
@@ -3,13 +3,16 @@ from django.contrib import admin
 from twilio.models import Compliance, MessagingService, PhoneNumber
 from django.contrib import admin
 
+
 @admin.register(PhoneNumber)
 class PhoneNumberAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(MessagingService)
 class MessagingServiceAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Compliance)
 class ComplianceAdmin(admin.ModelAdmin):
--- twilio/models.py
+++ twilio/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 class MessagingService(models.Model):
     sid = models.CharField(max_length=34)
     friendly_name = models.CharField(max_length=255)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    status =  models.CharField(max_length=16)
+    status = models.CharField(max_length=16)
 
     inbound_request_url = models.CharField(max_length=255)
     inbound_method = models.CharField(max_length=4)
@@ -22,17 +23,18 @@ class MessagingService(models.Model):
     def __str__(self):
         return self.friendly_name
 
+
 class Compliance(models.Model):
 
     sid = models.CharField(max_length=34)
     friendly_name = models.CharField(max_length=255)
-    has_embedded_phone:  models.BooleanField(default=False)
+    has_embedded_phone: models.BooleanField(default=False)
     brand_registration_sid: models.CharField(max_length=34)
     description: models.CharField(max_length=255)
     # message_samples: List[str]
     date_updated: models.DateTimeField(auto_now=True)
     campaign_status: models.CharField(max_length=16)
-    # rate_limits: 
+    # rate_limits:
     campaign_id: models.CharField(max_length=34)
     is_externally_registered: models.BooleanField(default=False)
     has_embedded_links: models.BooleanField(default=False)
@@ -41,7 +43,10 @@ class Compliance(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     us_app_to_person_usecase = models.CharField(max_length=255)
     mock = models.BooleanField(default=False)
-    messaging_service = models.ForeignKey(MessagingService, on_delete=models.CASCADE, null=True)
+    messaging_service = models.ForeignKey(
+        MessagingService, on_delete=models.CASCADE, null=True
+    )
+
 
 class PhoneNumber(models.Model):
     phone_number = models.CharField(max_length=20)
@@ -55,8 +60,10 @@ class PhoneNumber(models.Model):
     sms_url = models.CharField(max_length=255)
     status_callback = models.CharField(max_length=255)
     status_callback_method = models.CharField(max_length=4)
-    status =  models.CharField(max_length=16)
-    messaging_service = models.ForeignKey(MessagingService, on_delete=models.CASCADE, null=True)
+    status = models.CharField(max_length=16)
+    messaging_service = models.ForeignKey(
+        MessagingService, on_delete=models.CASCADE, null=True
+    )
 
     def __str__(self):
         return self.friendly_name
